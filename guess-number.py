1cd2aa2b9cdfed969fbd3bc94316ac1fc64ae120
Valley <vaaleyou@gmail.com>
2023-11-11 02:36:27 +0100

    Refactor equation generation and checking logic to improve code readability and optimize performance
    - Changed import statement to import specific functions from the operator module
    - Replaced explicit dictionary lookup for operators with direct function calls
    - Created a constant for number ranges based on the operator
    - Extracted logic to generate random numbers into a separate function
    - Reorganized code into game rounds and refactored input handling and result printing logic
    - Added welcome message and instructions for the game
    - Improved error handling for invalid user input
    - Removed unnecessary comments and unused code
    - Optimized the Timer class's __exit__ method
    
    The code changes in this commit improve the readability and efficiency of the equation generation and checking logic in the guess-number game.

--- guess-number.py
+++ guess-number.py
@@ -1,62 +1,59 @@
 import random
 import time
-import operator
+from operator import add, sub, mul
 
 OPERATORS = {
-    '+': operator.add,
-    '-': operator.sub,
-    '*': operator.mul
+    '+': add,
+    '-': sub,
+    '*': mul
 }
 
-def generate_and_check_equation():
-    # Generate a random operator (+, -, *)
-    operator_symbol = random.choice(list(OPERATORS.keys()))
+NUMBER_RANGES = {
+    '*': (1, 10),
+    '+': (1, 100),
+    '-': (1, 100)
+}
 
-    # Generate two random numbers
-    # For multiplication, numbers are between 1 and 10
-    # For addition and subtraction, numbers are between 1 and 100
-    if operator_symbol == '*':
-        num1 = random.randint(1, 10)
-        num2 = random.randint(1, 10)
-    else:
-        num1 = random.randint(1, 100)
-        num2 = random.randint(1, 100)
+def generate_random_number(operator):
+    return random.randint(*NUMBER_RANGES[operator])
 
-    # Calculate the correct answer
-    correct_answer = OPERATORS[operator_symbol](num1, num2)
-
-    # Create the equation string
-    equation = f"{num1} {operator_symbol} {num2}"
-
-    # Ask the user to solve the equation
+def get_user_answer(equation):
     while True:
         try:
-            user_answer = int(input(f"Solve the equation: {equation} = "))
-            break
+            return int(input(f"Solve the equation: {equation} = "))
         except ValueError:
             print("Invalid input. Please enter a number.")
 
-    # Check if the user's answer is correct
-    return user_answer == correct_answer
+def check_answer(num1, num2, operator_symbol, user_answer):
+    correct_answer = OPERATORS[operator_symbol](num1, num2)
+    return user_answer == correct_answer, correct_answer
 
+def game_round():
+    operator_symbol = random.choice(list(OPERATORS.keys()))
+    num1 = generate_random_number(operator_symbol)
+    num2 = generate_random_number(operator_symbol)
+    equation = f"{num1} {operator_symbol} {num2}"
+    user_answer = get_user_answer(equation)
+    is_correct, correct_answer = check_answer(num1, num2, operator_symbol, user_answer)
+    if is_correct:
+        print("✅ Correct!")
+    else:
+        print(f"❌ Incorrect! The correct answer was {correct_answer}.")
+    return is_correct
 
 def start_game():
-    # Initialize the number of correct answers to 0
-    correct_answers = 0
+    print("Hi and welcome to guess the number! In this game, you'll have to answer 10 math equations correctly as fast as possible.")
+    input("Press Enter when you are ready to start...")
 
-    # Start the timer
+    correct_answers = 0
     with Timer() as t:
         for _ in range(10):
-            if generate_and_check_equation():
-                print("✅ Correct!")
+            if game_round():
                 correct_answers += 1
-            else:
-                print("❌ Incorrect!")
 
     print_result(correct_answers, t.elapsed)
 
 def print_result(correct_answers, time_taken):
-    # Print the results
     if correct_answers == 10:
         print("🏆 You got all the equations correct!")
     elif correct_answers > 5:
@@ -70,19 +67,9 @@ class Timer:
         self.start = time.time()
         return self
 
-    def __exit__(self, *args):
+    def __exit__(self, exc_type, exc_val, exc_tb):
         self.end = time.time()
         self.elapsed = self.end - self.start
 
 if __name__ == "__main__":
-    print("Hi and welcome to guess the number! In this game, you'll have to answer 10 math equations correctly as fast as possible.")
-    while True:
-        user_input = input("Are you ready to start the game? (y/n) ")
-        if user_input.lower() == "y":
-            start_game()
-        elif user_input.lower() == "n":
-            print("Exiting in 3 seconds...")
-            time.sleep(3)
-            break
-        else:
-            print("Invalid input, please try again")
+    start_game()
